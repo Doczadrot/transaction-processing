0012a2ae4ce07efa3c79edd53f649d31c01bf220
Doczadrot <155732866+Doczadrot@users.noreply.github.com>
2024-01-31 18:58:53 +0300

    Синхронизация с удаленным репозиторием

--- src/main.py
+++ src/main.py
@@ -1,20 +1,39 @@
 import json
 
+def mask_card_number(card_number):
+    # Маскировка номера карты
+    return f"{card_number[:6]} {'*'*4} {card_number[-4:]}"
+
+def mask_account_number(account_number):
+    # Маскировка номера счета
+    return f"**{account_number[-4:]}"
+
 def process_transactions(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
-    for transaction in data:
-        print(f"Transaction ID: {transaction['id']}")
-        print(f"Date: {transaction['date']}")
-        print(f"State: {transaction['state']}")
-        print(f"Operation Amount: {transaction['operationAmount']}")
-        print(f"Description: {transaction['description']}")
-        print(f"From: {transaction.get('from', 'N/A')}")
-        print(f"To: {transaction['to']}")
-        print("\n")
+    # Фильтрация и сортировка транзакций
+    executed_transactions = [t for t in data if t.get('state') == 'EXECUTED']
+    sorted_transactions = sorted(executed_transactions, key=lambda t: t.get('date'), reverse=True)[:5]
+
+    for transaction in sorted_transactions:
+        date = transaction.get('date', 'N/A').split('T')[0]
+        description = transaction.get('description', 'N/A')
+        from_account = transaction.get('from', 'N/A')
+        to_account = transaction.get('to', 'N/A')
+        amount = transaction.get('operationAmount', {}).get('amount', 'N/A')
+        currency = transaction.get('operationAmount', {}).get('currency', {}).get('name', 'N/A')
+
+        # Маскировка номеров счетов и карт
+        masked_from_account = mask_card_number(from_account) if from_account else from_account
+        masked_to_account = mask_account_number(to_account)
+
+        # Вывод транзакции
+        print(f"{date} {description}")
+        print(f"{masked_from_account} -> Счет {masked_to_account}")
+        print(f"{amount} {currency}\n")
+
 
 if __name__ == "__main__":
-    # Измените путь к файлу на вашем компьютере
-    file_path = r"C:\Users\ivani\OneDrive\Рабочий стол\Гладченко И Г\operations (1).json"
+    file_path = "operations.json"
     process_transactions(file_path)
